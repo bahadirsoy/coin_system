02a67e92f9c1994310218357de2faa4f7c505017
Bahadir Ustabasi <oxikairos@hotmail.com>
2021-07-27 13:07:16 +0300

    admin can add a departmant

--- Control/adminAddDepartmantControl.php
+++ Control/adminAddDepartmantControl.php
@@ -10,5 +10,28 @@ if (!isset($departmantName) || !isset($departmantName) || !isset($username) || !
     header("Location: ../adminAddDepartmant.php?result=empty&departmantName=".$departmantName."&username=".$username);
     exit();
 } else{
-    exit("asd");
+    $sql = "SELECT * FROM departman WHERE username='$username' OR departmantName='$departmantName'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+
+        $row = mysqli_fetch_assoc($result);
+        print_r($row);
+
+    } else if (mysqli_num_rows($result) == 0){
+
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO departman
+        VALUES (NULL, '$departmantName', '$username', '$hashed_password', '0')";
+
+        if ($conn->query($sql)) {
+            header("Location: ../adminAddDepartmant.php?result=success");
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+    } else{
+        echo "1 den fazla sonuç geldi SQL'de hata var.";
+    }
 }
